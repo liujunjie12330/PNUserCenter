5781978cb7269a08670716cbc451b3f7e07bbbee
javadadi <2830486579@qq.com>
2024-11-24 18:30:25 +0800

    [javadadi][基本请求路径 main 分支提交]

---
+++ common/src/main/java/com/pn/common/constant/PNUserCenterConstant.java
@@ -0,0 +1,14 @@
+package com.pn.common.constant;
+
+/**
+ * 系统相关常量
+ * @author: javadadi
+ * @Time: 18:28
+ * @ClassName: PNUserCenterConstant
+ */
+public interface PNUserCenterConstant {
+    /**
+     * 基本请求路径
+     */
+    static final String BASE_URL = "/v1/usercenter/server";
+}

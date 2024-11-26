2b7b371070bd6f14fbc2e90c1db73ea1aa5afe31
Umbrella886 <2362992795@qq.com>
2024-11-26 22:37:09 +0800

    [feat-feat-1003-tuminghua][all files][第三方登录-暂提交]

--- web/src/main/java/com/pn/web/controller/LoginController.java
+++ web/src/main/java/com/pn/web/controller/LoginController.java
@@ -6,12 +6,10 @@ import com.pn.common.enums.StatusCode;
 import com.pn.common.exception.BizException;
 import com.pn.common.params.login.UserLoginParams;
 import com.pn.common.utils.ResultUtils;
+import com.pn.service.pnservice.login.Login3rdTarget;
 import com.pn.service.pnservice.login.UserLoginService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -27,6 +25,9 @@ public class LoginController {
     @Resource
     private UserLoginService loginService;
 
+    @Resource
+    private Login3rdTarget login3rdTarget;
+
     @PostMapping("/login")
     public BaseResponse<String> login(@RequestBody UserLoginParams params, HttpServletResponse response){
      String username = params.getUsername();
@@ -36,6 +37,28 @@ public class LoginController {
          throw  new BizException(StatusCode.PASSWORD_ERROR);
      }
         String token = loginService.doLogin(username, password, code);
+        response.setHeader("token",token);
+        return ResultUtils.success("ok");
+    }
+
+    @PostMapping("/user/login/third")
+    public BaseResponse<String> loginByOAuth(@RequestParam String state, @RequestParam String code, HttpServletResponse response){
+        if(StringUtils.isBlank(state) ){
+            throw new BizException(StatusCode.PASSWORD_ERROR);
+        }
+        String token ="";
+        if("GITEE".equals(state)){
+            token = login3rdTarget.loginByGitee(code, state);
+        }
+        if( "GITHUB".equals(state)){
+            token = login3rdTarget.loginByGitHub(code,state);
+        }
+        if("qq".equals(state)){
+            token = login3rdTarget.loginByQQ("params");
+        }
+        if("wechat".equals(state)){
+            token = login3rdTarget.loginByWechat("params");
+        }
         response.setHeader("token",token);
         return ResultUtils.success("ok");
     }

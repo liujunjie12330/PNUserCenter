9d801c01cd3dc9999a75182c5703f8a69f6c33b8
javadadi <2830486579@qq.com>
2024-11-26 21:42:16 +0800

    数据库字段调整

--- sql/init/create.sql
+++ sql/init/create.sql
@@ -1,20 +1,22 @@
-# 用户表
 CREATE TABLE pn_user
 (
-    id               bigint AUTO_INCREMENT PRIMARY KEY,
-    create_by        bigint        NOT NULL COMMENT '创建人',
-    update_by        bigint        NOT NULL COMMENT '修改人',
-    create_at        datetime      NOT NULL COMMENT '创建时间',
-    update_at        datetime      NOT NULL COMMENT '修改时间',
-    is_deleted       tinyint(1)    NOT NULL COMMENT '是否删除（0,1）',
-    username         varchar(100)  NOT NULL COMMENT '用户名',
-    password         varchar(256)  NOT NULL COMMENT '用户密码',
-    full_name        varchar(128)  NOT NULL COMMENT '用户全名',
-    phone            varchar(255)  NOT NULL COMMENT '手机号',
-    email            varchar(1024) NOT NULL COMMENT '邮箱',
-    is_admin         varchar(1)    NOT NULL COMMENT '是否管理员',
-    last_login_date  datetime      NOT NULL COMMENT '最后一次登录时间',
-    update_last_time datetime      NOT NULL COMMENT '最后一次修改时间'
+    id               bigint PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
+    create_at        datetime     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+    update_at        datetime     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '最近修改时间',
+    create_by        bigint       DEFAULT -1                NOT NULL COMMENT '创建人',
+    update_by        bigint       DEFAULT -1                NOT NULL COMMENT '更新人',
+    is_deleted       tinyint(1)   DEFAULT 0                 NOT NULL COMMENT '是否删除1--删除',
+    username         varchar(100)                           NOT NULL COMMENT '用户名',
+    password         varchar(256)                           NOT NULL COMMENT '用户密码',
+    full_name        varchar(128)                           NOT NULL COMMENT '用户全名',
+    phone            varchar(255)                           NOT NULL COMMENT '手机号',
+    email            varchar(1024)                          NOT NULL COMMENT '邮箱',
+    is_admin         tinyint(1)                             NOT NULL COMMENT '是否管理员 1--是',
+    last_login_date  datetime                               NOT NULL COMMENT '最后一次登录时间',
+    update_last_time datetime     DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '最后一次修改时间',
+    github_id        varchar(255) DEFAULT '' COMMENT 'github登陆账号id',
+    gitee_id         varchar(255) DEFAULT '' COMMENT 'gitee登录帐号id',
+    gitlab_id        varchar(255) DEFAULT '' COMMENT 'gitlab登陆账号id'
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8 COMMENT '用户信息表';
